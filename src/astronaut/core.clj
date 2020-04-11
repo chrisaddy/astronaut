e5c70d274fe87ce0c0df81d3554483c7a0ffba9c
chrisaddy <chris.william.addy@gmail.com>
2020-04-11 13:39:52 -0400

    second commit

--- src/astronaut/core.clj
+++ src/astronaut/core.clj
@@ -1,7 +1,16 @@
 (ns astronaut.core
+  (:require [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
+
+(def cli-options
+  [[]
+   ["-h" "--help"]]
+  )
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  (:bin {:bin-path "~/bin" :bootclasspath true})
+  (parse-opts args cli-options)
+  (println (str cli-options))
   (println "Hello, World!"))
