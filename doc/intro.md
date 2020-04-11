513bf7e694b3a5995fc0fcfe6bd5cc0a36451d24
chrisaddy <chris.william.addy@gmail.com>
2020-04-11 12:48:44 -0400

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to astronaut
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/astronaut/core_test.clj
@@ -0,0 +1,7 @@
+(ns astronaut.core-test
+  (:require [clojure.test :refer :all]
+            [astronaut.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
